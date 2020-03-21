c0a16f90e2a51a23c8e057e7b16da404b7ca8d79
Hervé Boutemy <hboutemy@apache.org>
2020-03-21 18:45:29 +0100

    added org.apache.sling.commons.johnzon

---
+++ org/apache/sling/org.apache.sling.commons.johnzon/analyze.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+repo=https://repo.maven.apache.org/maven2
+groupId=org.apache.sling    
+artifactId=org.apache.sling.commons.johnzon
+groupDir=$(echo ${groupId} | tr '.' '/')
+
+scm=https://github.com/apache/sling-$(echo ${artifactId} | tr '.' '-').git
+
+versionsWithoutTag=""
+versions="1.0.0 1.1.0 1.1.2 1.2.0"
+
+mkdir --parents target
+pushd target > /dev/null
+[ -d ${artifactId} ] || git clone ${scm}
+
+mkdir --parents central
+cd central
+for version in ${versionsWithoutTag} ${versions}
+do
+  [ -f ${artifactId}-${version}.jar.sha1 ] || wget ${repo}/${groupDir}/${artifactId}/${version}/${artifactId}-${version}.jar.sha1
+  [ -f ${artifactId}-${version}.jar ] || wget ${repo}/${groupDir}/${artifactId}/${version}/${artifactId}-${version}.jar
+  [ -f ${artifactId}-${version}.pom ] || unzip -p ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml > ${artifactId}-${version}.pom
+done
+
+for version in ${versionsWithoutTag} ${versions}
+do
+  pomDate=$(unzip -v ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml | grep "META-INF/maven/${groupId}/${artifactId}/pom.xml" | sed 's/.*\(....-..-..\).*/\1/')
+  outputTimestamp=$(unzip -p ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml | grep "<project.build.outputTimestamp>" | sed 's/^.*<project.build.\(outputTimestamp>[^<]+\).*$/\1/')
+  buildJdk=$(unzip -p ${artifactId}-${version}.jar META-INF/MANIFEST.MF | dos2unix | grep "Build-Jdk")
+  unzip -p ${artifactId}-${version}.jar org/apache/maven/plugins/dependency/AbstractDependencyMojo.class > ../tmp.class 2> /dev/null
+  unzip -p ${artifactId}-${version}.jar org/apache/maven/plugin/dependency/AbstractDependencyMojo.class >> ../tmp.class 2> /dev/null
+  target=$(file -b ../tmp.class | cut -c 27-)
+  #createdBy=$(unzip -p ${artifactId}-${version}.jar META-INF/MANIFEST.MF | dos2unix | grep "Created-By")
+
+  win="" && $(grep -q $'\r' ${artifactId}-${version}.pom) && win=", built on Windows"
+
+  echo -e "${pomDate}   ${version}\t${buildJdk}, bytecode ${target}${win} ${outputTimestamp}"
+done
+
+popd > /dev/null
