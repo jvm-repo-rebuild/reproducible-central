9b7421f6d7c597fc4be9bf8c0a7c0db1684d7379
Hervé Boutemy <hboutemy@apache.org>
2020-03-21 17:59:11 +0100

    added jpdftest

---
+++ io/github/derkrischan/jpdftest/analyze.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+repo=https://repo.maven.apache.org/maven2
+groupId=io.github.derkrischan
+artifactId=jpdftest
+groupDir=$(echo ${groupId} | tr '.' '/')
+
+scm=https://github.com/derKrischan/${artifactId}.git
+
+versionsWithoutTag=""
+versions="0.1.0 0.3.0 0.5.0 0.6.0 0.8.0"
+
+mkdir --parents target
+pushd target > /dev/null
+[ -d ${artifactId} ] || git clone ${scm}
+
+mkdir --parents central
+cd central
+for version in ${versionsWithoutTag} ${versions}
+do
+  [ -f ${artifactId}-${version}.jar.sha1 ] || wget ${repo}/${groupDir}/${artifactId}/${version}/${artifactId}-${version}.jar.sha1
+  [ -f ${artifactId}-${version}.jar ] || wget ${repo}/${groupDir}/${artifactId}/${version}/${artifactId}-${version}.jar
+  [ -f ${artifactId}-${version}.pom ] || unzip -p ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml > ${artifactId}-${version}.pom
+done
+
+for version in ${versionsWithoutTag} ${versions}
+do
+  pomDate=$(unzip -v ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml | grep "META-INF/maven/${groupId}/${artifactId}/pom.xml" | sed 's/.*\(....-..-..\).*/\1/')
+  outputTimestamp=$(unzip -p ${artifactId}-${version}.jar META-INF/maven/${groupId}/${artifactId}/pom.xml | grep "<project.build.outputTimestamp>" | sed 's/^.*<project.build.\(outputTimestamp>[^<]+\).*$/\1/')
+  buildJdk=$(unzip -p ${artifactId}-${version}.jar META-INF/MANIFEST.MF | dos2unix | grep "Build-Jdk")
+  unzip -p ${artifactId}-${version}.jar io/github/derkrischan/pdftest/AbstractPdfAssert.class > ../tmp.class
+  target=$(file -b ../tmp.class | cut -c 27-)
+  #createdBy=$(unzip -p ${artifactId}-${version}.jar META-INF/MANIFEST.MF | dos2unix | grep "Created-By")
+
+  win="" && $(grep -q $'\r' ${artifactId}-${version}.pom) && win=", built on Windows"
+
+  echo -e "${pomDate}   ${version}\t${buildJdk}, bytecode ${target}${win} ${outputTimestamp}"
+done
+
+popd > /dev/null
