fb2482a44e28bdea82bc0ac7d0a9d789535f380b
Hervé Boutemy <hboutemy@apache.org>
2020-10-17 16:03:25 +0200

    add br to column title

--- update-reproducibility-summary.sh
+++ update-reproducibility-summary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat <(echo "| [Central Repository](https://search.maven.org/) groupId:artifactId(s) | version | [build spec](BUILDSPEC.md) | [result](https://reproducible-builds.org/docs/jvm/): reproducible? |"
+cat <(echo "| [Central Repository](https://search.maven.org/) groupId:artifactId(s) | version | [build spec](BUILDSPEC.md) | [result](https://reproducible-builds.org/docs/jvm/):<br/>reproducible? |"
 echo "| -------------------------------- | -- | --------- | ------ |"
 
 anchor="empty"
