75eef8ee469c80d3c4ca6e02c6820c5c1b30408d
Dan Rollo <danrollo@gmail.com>
2021-08-05 16:44:14 -0400

    adjust for sed command on mac

--- build_diffoscope.sh
+++ build_diffoscope.sh
@@ -7,7 +7,13 @@ diffoscope_file=$(dirname ${compare})/$(basename ${compare} .buildinfo.compare).
 
 echo -e "saving build diffoscope file to \033[1m${diffoscope_file}\033[0m"
 
-grep '# diffoscope ' ${compare} | sed -e 's/# diffoscope //' | ( while read -r line
+set sed="sed"
+if [ "$(uname -s)" ==  "Darwin" ]
+then
+  sed="gsed"
+fi
+
+grep '# diffoscope ' ${compare} | ${sed} -e 's/# diffoscope //' | ( while read -r line
 do
   echo -e "\033[1m$line\033[0m"
   docker run --rm -t -w /mnt -v $(pwd)/$(dirname ${compare})/${builddir}:/mnt:ro registry.salsa.debian.org/reproducible-builds/diffoscope --no-progress $line
@@ -15,7 +21,7 @@ do
 done ) | tee ${diffoscope_file}
 
 # remove ansi escape codes from file
-sed -i 's/\x1b\[[0-9;]*m//g' ${diffoscope_file}
+${sed} -i 's/\x1b\[[0-9;]*m//g' ${diffoscope_file}
 
 echo -e "build diffoscope file saved to \033[1m${diffoscope_file}\033[0m"
 du -h ${diffoscope_file}
