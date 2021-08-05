ea5a2bc4fe2f73c187413c1bad0119295a9067f3
Dan Rollo <danrollo@gmail.com>
2021-08-05 10:39:21 -0400

    move readme diff check to script

---
+++ .circleci/commit-readme.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "*** running script: $0"
+
+export LC_ALL=C
+
+git add README.md
+git status --porcelain
+
+git diff --exit-code README.md
+statusCheck=$?
+echo "git diff: $statusCheck"
+
+if [ $statusCheck -eq 0 ]; then
+  echo "Changes detected. committing README.md"
+#  git commit -m "nigthly update of README.md [skip ci]"
+else
+  echo "No changes detected. skipping commit"
+fi
