b009df6a5bf36da454709955e4d9bd26866fd40c
Dan Rollo <danrollo@gmail.com>
2021-08-05 10:51:29 -0400

    local debugging, move add

--- .circleci/commit-readme.sh
+++ .circleci/commit-readme.sh
@@ -4,16 +4,16 @@ echo "*** running script: $0"
 
 export LC_ALL=C
 
-git add README.md
 git status --porcelain
 
 git diff --exit-code README.md
 statusCheck=$?
 echo "git diff: $statusCheck"
 
-if [[ $statusCheck -eq 0 ]]; then
-  echo "Changes detected. committing README.md"
-#  git commit -m "nightly update of README.md [skip ci]"
+if [[ $statusCheck -ne 0 ]]; then
+  echo "Changes detected. committing README.md."
+  git add README.md
+  git commit -m "nightly update of README.md [skip ci]"
 else
-  echo "No changes detected. skipping commit"
+  echo "No changes detected. skipping commit."
 fi
