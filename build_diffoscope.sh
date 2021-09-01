bb8cfbdf66c37caa540c116a917fc2f51f68d754
ForgQi <34411314+ForgQi@users.noreply.github.com>
2021-09-02 02:21:01 +0800

    Update build_diffoscope.sh

--- build_diffoscope.sh
+++ build_diffoscope.sh
@@ -7,7 +7,7 @@ diffoscope_file=$(dirname ${compare})/$(basename ${compare} .buildinfo.compare).
 
 echo -e "saving build diffoscope file to \033[1m${diffoscope_file}\033[0m"
 
-set sed="sed"
+sed="sed"
 if [ "$(uname -s)" ==  "Darwin" ]
 then
   command -v gsed >/dev/null 2>&1 || { echo "require GNU sed: brew install gsed  Aborting."; exit 1; }
