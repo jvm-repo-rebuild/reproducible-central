3e55471f5227292d6b6df03ad03cd256d9270713
Hervé Boutemy <hboutemy@apache.org>
2021-11-27 19:47:03 +0100

    add checksum-maven-plugin 1.11

--- rebuild.sh
+++ rebuild.sh
@@ -141,6 +141,7 @@ mvnBuildLocal() {
 rebuildToolMvn() {
   # the effective rebuild command, adding artifact:buildinfo goal to compare with central content
   local mvn_rebuild="${command} -V -e artifact:compare -Dbuildinfo.reproducible"
+  #local mvn_rebuild="${command} -V -e org.apache.maven.plugins:maven-artifact-plugin:3.2.0:compare -Dbuildinfo.reproducible"
 
   # by default, build with Docker
   # TODO: on parameter, use instead mvnBuildLocal after selecting JDK
@@ -151,7 +152,7 @@ rebuildToolMvn() {
   dos2unix ${buildinfo} || fatal "failed to convert buildinfo newlines"
   cp ${buildinfo} ../.. || fatal "failed to copy buildinfo file"
 
-  buildcompare="$(basename ${buildinfo} .buildinfo).buildcompare"
+  buildcompare="$(dirname "${buildinfo}")/$(basename ${buildinfo} .buildinfo).buildcompare"
   dos2unix ${buildcompare} || fatal "failed to convert buildcompare newlines"
   cp ${buildcompare} ../.. || fatal "failed to copy buildcompare file"
 
