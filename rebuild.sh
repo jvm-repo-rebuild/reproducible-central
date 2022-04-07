10cc72662bb859b1dbdf195bc0cc724dee420e2a
Hervé Boutemy <hboutemy@apache.org>
2022-04-07 07:44:50 +0200

    switch maven-artifact-plugin to 3.3.0

--- rebuild.sh
+++ rebuild.sh
@@ -150,7 +150,7 @@ mvnBuildLocal() {
 rebuildToolMvn() {
   # the effective rebuild command, adding artifact:buildinfo goal to compare with central content
   #local mvn_rebuild="${command} -V -e artifact:compare -Dbuildinfo.reproducible"
-  local mvn_rebuild="${command} -V -e org.apache.maven.plugins:maven-artifact-plugin:3.2.0:compare -Dbuildinfo.reproducible"
+  local mvn_rebuild="${command} -V -e org.apache.maven.plugins:maven-artifact-plugin:3.3.0:compare -Dbuildinfo.reproducible"
 
   # by default, build with Docker
   # TODO: on parameter, use instead mvnBuildLocal after selecting JDK
