91bab76cce3e3543bea5ca4da864311612aec291
Hervé Boutemy <hboutemy@apache.org>
2022-06-18 07:56:56 +0200

    not only main README.md is interesting to commit

--- .circleci/commit-readme.sh
+++ .circleci/commit-readme.sh
@@ -6,14 +6,14 @@ export LC_ALL=C
 
 git status --porcelain
 
-git diff --exit-code README.md
+git diff --exit-code
 statusCheck=$?
 echo "git diff: $statusCheck"
 
 if [[ $statusCheck -ne 0 ]]; then
-  echo "Changes detected. committing README.md."
+  echo "Changes detected, committing."
   git add -A
-  git commit -m "nightly update of README.md [skip ci]"
+  git commit -m "nightly update of content [skip ci]"
 else
-  echo "No changes detected. skipping commit."
+  echo "No changes detected, skipping commit."
 fi
