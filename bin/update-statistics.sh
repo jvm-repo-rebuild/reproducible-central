00046b627a1a128d4dbcee7839e44b63aabd7cfc
Hervé Boutemy <hboutemy@apache.org>
2022-10-23 16:20:15 +0200

    add buildspec stats 2020-04-01 83dda39ba3184736245c0e641fe872ce77329c48

---
+++ bin/update-statistics.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+echo -n > tmp-tool-jdk
+echo -n > doc/buildspecs.txt
+
+tool=mvn
+
+for buildspec in $(find content -name "*.buildspec" -print)
+do
+  . ${buildspec}
+  printf "%-9s %3d\n" ${tool} ${jdk} >> tmp-tool-jdk
+  printf "%-9s %3d ${buildspec}\n" ${tool} ${jdk} >> doc/buildspecs.txt
+done
+
+cat tmp-tool-jdk | wc -l > doc/buildspec-stats.txt
+sort tmp-tool-jdk | uniq -c >> doc/buildspec-stats.txt
+
+# Docker image per maven tool configuration as defined in rebuild.sh
+mvnImage() {
+  local tool jdk
+  tool="$1"
+  jdk="$2"
+
+case ${jdk} in
+  6 | 7)
+    mvnImage=maven:3.6.1-jdk-${jdk}-alpine
+    ;;
+  *)
+    mvnImage=maven:3.6.3-jdk-${jdk}-slim
+esac
+}
+
+sort tmp-tool-jdk | uniq | grep "mvn" | while read val
+do
+  mvnImage $val
+  echo "$val $mvnImage" >> doc/buildspec-stats.txt
+done
+
+rm tmp-tool-jdk
+cat doc/buildspec-stats.txt
