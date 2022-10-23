b890b8cf58decca193fccc8cee1378c1a2f43db6
Hervé Boutemy <hboutemy@apache.org>
2022-10-23 16:46:41 +0200

    add buildspec stats 2022-10-01 a94b1afae5cd16d298737c9b8b8f22d4ff5a81af

--- bin/update-statistics.sh
+++ bin/update-statistics.sh
@@ -19,28 +19,39 @@ sort tmp-tool-jdk | uniq -c >> doc/buildspec-stats.txt
 
 # Docker image per maven tool configuration as defined in rebuild.sh
 mvnImage() {
-  local tool jdk
+  local tool jdk mvnVersion
   tool="$1"
   jdk="$2"
 
+  mvnVersion="3.6.3"
+  case ${jdk} in
+    18)
+      mvnVersion="3"
+  esac
+  case ${tool} in
+    mvn-*)
+      mvnVersion="$(echo "$tool" | cut -d - -f 2)"
+  esac
+
+  # select Docker image to match required JDK version: https://hub.docker.com/_/maven
   case ${jdk} in
     6 | 7)
       mvnImage=maven:3.6.1-jdk-${jdk}-alpine
       ;;
     8)
-      mvnImage=maven:3.6.3-jdk-${jdk}-slim
+      mvnImage=maven:${mvnVersion}-jdk-${jdk}-slim
       ;;
     9)
       mvnImage=maven:3-jdk-${jdk}-slim
       ;;
     14)
-      mvnImage=maven:3.6.3-jdk-${jdk}
+      mvnImage=maven:${mvnVersion}-jdk-${jdk}
       ;;
     15 | 16 | 17)
-      mvnImage=maven:3.6.3-openjdk-${jdk}-slim
+      mvnImage=maven:${mvnVersion}-openjdk-${jdk}-slim
       ;;
     *)
-      mvnImage=maven:3.6.3-jdk-${jdk}-slim
+      mvnImage=maven:${mvnVersion}-openjdk-${jdk}-slim
   esac
 }
 
