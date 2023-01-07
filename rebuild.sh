7b74301a267d005c2c281575bcf23564acb02616
Marc Wrobel <marc.wrobel@gmail.com>
2023-01-07 19:21:17 +0100

    fix the move command in rebuild.sh
    Removing `${buildinfo}` from the move commande : `wip/$(basename ${buildinfo} .buildinfo).build*` is already matching all the files.

--- rebuild.sh
+++ rebuild.sh
@@ -243,7 +243,7 @@ displayResult() {
 
   if [[ ${buildspec} == wip/* ]]
   then
-    echo -e "\033[93mWork In Progress\033[0m: once work ready to publish, move to target directory with \033[1mdir=content/$(echo ${groupId} | tr '.' '/')/${artifactId} ; mkdir -p \${dir} ; mv ${buildspec} wip/$(basename ${buildinfo} .buildinfo).build* \${dir} \033[0m"
+    echo -e "\033[93mWork In Progress\033[0m: once work is ready to be published, move it to target directory with \033[1mdir=content/$(echo ${groupId} | tr '.' '/')/${artifactId} ; mkdir -p \${dir} ; mv wip/$(basename ${buildinfo} .buildinfo).build* \${dir} \033[0m"
   fi
 }
 
