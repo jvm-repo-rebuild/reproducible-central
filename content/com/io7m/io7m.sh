b0f516bcc713f25fb8c9f80b996a3705e25a86d4
Hervé Boutemy <hboutemy@apache.org>
2023-08-26 22:15:53 +0200

    add io7m init script

---
+++ content/com/io7m/io7m.sh
@@ -0,0 +1,11 @@
+#!bash
+
+p=$1
+v=$2
+
+[ -d $p ] || mkdir $p
+cat canonmill/com.io7m.canonmill-1.0.0.buildspec | sed -e "s/canonmill/$p/" | sed -e "s/1.0.0/$v/" | sed -e 's/ -Dassembly.skipAssembly//' > $p/com.io7m.$p-$v.buildspec
+
+echo "./rebuild.sh content/com/io7m/$p/com.io7m.$p-$v.buildspec"
+echo "git add $p ; git commit -m 'add com.io7m.$p $v' ; git push"
+echo "bin/add-new-release.sh content/com/io7m/$p/com.io7m.$p-$v.buildspec $v"
