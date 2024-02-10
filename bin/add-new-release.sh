5a2dcab74d35e2f89ac6044f68ecca9d522db8ee
Hervé Boutemy <hboutemy@apache.org>
2024-02-10 11:24:33 +0100

    put commmand to copy/paste on its own line

--- bin/add-new-release.sh
+++ bin/add-new-release.sh
@@ -15,4 +15,5 @@ sed "s/^version=.*/version=${nextVersion}/" ${previousBuildspec} > ${nextBuildsp
 ./rebuild.sh ${nextBuildspec}
 
 echo
-echo -e "if result is ok, run: \033[1mgit add ${dir} ; git commit -m \"add ${artifactId} ${nextVersion}\" ; git push\033[0m"
+echo -e "if result is ok, run:"
+echo -e "\e[0;32mgit add ${dir} ; git commit -m \"add ${artifactId} ${nextVersion}\" ; git push\e[0m"
