cfa436665a28b579b755b655914b838470f906fc
Hervé Boutemy <hboutemy@apache.org>
2024-09-10 08:00:23 +0200

    drop leading 1. from old JDK versions

--- bin/add-new-release.sh
+++ bin/add-new-release.sh
@@ -16,11 +16,17 @@ previousBuildinfo=$(ls $dir | grep "\-${version}\.buildinfo")
 previousJar="$(grep "\-${version}\.jar" $dir/$previousBuildinfo | head -1 | cut -d = -f 2)"
 previousArtifactId="$(echo "$previousJar" | sed -e "s/-${version}.jar$//")"
 nextJar="$previousArtifactId-$nextVersion.jar"
+
+echo "detecting JDK from $nextJar downloaded from https://repo.maven.apache.org/maven2/$(echo ${groupId} | tr '.' '/')/${previousArtifactId}/${nextVersion}/"
 [ -d tmp ] || mkdir tmp
 [ -f tmp/$nextJar ] || curl -s --fail https://repo.maven.apache.org/maven2/$(echo ${groupId} | tr '.' '/')/${previousArtifactId}/${nextVersion}/$nextJar --output tmp/$nextJar
+unzip -q -c tmp/$nextJar META-INF/MANIFEST.MF > tmp/$previousArtifactId-$nextVersion-MANIFEST.MF
+unzip -q -c tmp/$nextJar META-INF/maven/$groupId/$previousArtifactId/pom.properties > tmp/$previousArtifactId-$nextVersion-pom.properties
+unzip -q -c tmp/$nextJar META-INF/maven/$groupId/$previousArtifactId/pom.xml > tmp/$previousArtifactId-$nextVersion-pom.xml
+du --apparent-size -h tmp/$previousArtifactId-$nextVersion*
 
-nextJdk="$(unzip -q -c tmp/$nextJar META-INF/MANIFEST.MF | grep Jdk | cut -d ' ' -f 2)"
-[ "$jdk" != "$nextJdk" ] && echo "jdk $jdk => $nextJdk" && sed -i "s/^jdk=.*/jdk=${nextJdk}/" ${nextBuildspec}
+nextJdk="$(unzip -q -c tmp/$nextJar META-INF/MANIFEST.MF | grep Jdk | cut -d ' ' -f 2 | sed -e 's/^1\.//')"
+[ "$jdk" != "$nextJdk" ] && echo -e "\033[0;1mupdating jdk: $jdk => $nextJdk\033[0;0m" && sed -i "s/^jdk=.*/jdk=${nextJdk}/" ${nextBuildspec}
 
 ./rebuild.sh ${nextBuildspec} $3
 
