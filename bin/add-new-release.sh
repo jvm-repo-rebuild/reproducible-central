871a7facdb35cda7a117fea57f43726692bf7da7
Hervé Boutemy <hboutemy@apache.org>
2024-09-29 18:42:01 +0200

    propose gh pr create command

--- bin/add-new-release.sh
+++ bin/add-new-release.sh
@@ -94,10 +94,9 @@ echo -e "test local build with: \033[0;32m./rebuild.sh ${nextBuildspec} $3\033[0
 
 echo
 echo -e "once buildspec is ok:"
-echo -e "1. either run:"
-echo -e "\033[0;32mgit checkout -b $artifactId-$nextVersion ; git add ${nextBuildspec} ; git commit -m 'add ${artifactId} ${nextVersion}' -m '' -m 'https://github.com/jvm-repo-rebuild/reproducible-central/blob/master/${dir}/README.md' ; git push --set-upstream origin $artifactId-$nextVersion\033[0m"
-echo -e "  then create a PR"
-echo -e "2. or commit directly to master by running:"
+echo -e "1. either create a PR:"
+echo -e "\033[0;32mgit checkout -b $artifactId-$nextVersion ; git add ${nextBuildspec} ; git commit -m 'add ${artifactId} ${nextVersion}' -m '' -m 'https://github.com/jvm-repo-rebuild/reproducible-central/blob/master/${dir}/README.md' ; git push --set-upstream origin $artifactId-$nextVersion ; gh pr create --fill --head $artifactId-$nextVersion\033[0m"
+echo -e "2. or commit directly to master:"
 echo -e "\033[0;32mbin/update-project-summary.sh ${dir} ; git add ${dir} ; git commit -m \"add ${artifactId} ${nextVersion}\" ; git push\033[0m"
 echo -e "3. or drop the local test:"
 echo -e "\033[0;32mrm ${dir}/*-${nextVersion}\.*\033[0m"
